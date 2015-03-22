d9bc7d59fb064d2c1f90cde014f7e5de4933bd3f
Chad H <chadroberthall@gmail.com>
2015-03-22 15:08:23 -0600

    Lets do this

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -11,24 +11,37 @@
  */
 
 namespace Controllers;
-
+use Common\Authentication\UserThatsFileLoaded;
+use Common\Authentication\UserStoredInMemory;
+use Common\Authentication\UserFromMySQL;
 
 /**
  * Class AuthController
  */
 class AuthController extends Controller
 {
-    /**
-     * Function execute
-     *
-     * @access public
-     */
+    
     public function action()
     {
         $postData = $this->request->getPost();
 
-        echo 'Authenticate the above two different ways' . var_dump($postData);
 
-        // example code: $auth = new Authentication($postData['username'], $postData['password']);
+        if ($postData->authSelect =='FileBased') {
+            $loggingUser = new UserThatsFileLoaded($postData);
+        }
+        else if ($postData->authSelect =='InMemory'){
+            $loggingUser = new UserStoredInMemory($postData) ;
+        }
+        else if ($postData->authSelect =='MySQL'){
+            $loggingUser = new UserFromMySQL($postData) ;
+        }
+        if ($loggingUser->authenticate()) {
+             echo '<br><h1>'."You have successfully Logged IN. FINALLY!!! It worked!!!".'</h1>';       
+        } else {
+             echo '<br><h1>'."No access! Please try again buddy!".'</h1>';
+        }
+
+
+
     }
 }
--- src/Views/LoginForm.php
+++ src/Views/LoginForm.php
@@ -17,9 +17,19 @@ class LoginForm extends View
     <body>
         <div align="center">
             <form method="POST" action="/auth">
-                Username: <input type="text" name="username" size="15" /><br />
+                Username:
+                <input type="text" name="username" size="15" /><br />
                 Password: <input type="password" name="password" size="15" /><br />
-                <p><input type="submit" value="Login" /></p>
+                
+                Authorization Choice: 
+                <div class="radio">
+                    <label>
+                        <input type="radio" name="authSelect" value="InMemory" checked> In Memory
+                        <input type="radio" name="authSelect" value="FileBased"> File Based
+                        <input type="radio" name="authSelect" value="MySQL"> MySQL Based
+                    </label>
+                </div>
+                <p><input type="submit" value="Login" /></p><br />
             </form>
         </div>
     </body>
